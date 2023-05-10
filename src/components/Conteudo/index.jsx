7200a0d822dc9075fcfd202fc2c7a5fac3d730c4
Fagner Cangussu <117116767+fagnerrc@users.noreply.github.com>
2023-05-10 12:29:42 -0300

    Update index.jsx

--- src/components/Conteudo/index.jsx
+++ src/components/Conteudo/index.jsx
@@ -2,7 +2,7 @@ import './index.css';
 export function Conteudo (){
     return(
     <div className='conteudo'>
-        <h1>Olá! Eu sou o Fagner</h1>
+        <h1>Sou o Fagner e o Gabriel e meu AMOR</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis in orci sed scelerisque. Mauris erat nibh, dignissim sed massa vehicula, maximus imperdiet sapien. Sed id dui sapien. Sed viverra non nisi eget ultricies. Nulla laoreet consectetur mauris, vestibulum laoreet ligula elementum eget. Nullam ante tellus, luctus eget mi vel, commodo tincidunt odio. Suspendisse laoreet erat leo, eget egestas orci vestibulum vitae. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse auctor egestas est et pulvinar. Aliquam finibus sem id semper volutpat.
         </p>
